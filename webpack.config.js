206f5404bc7a845c7043f88b5c978b5667ce5268
slkcoin <xcoin@me.com>
2016-12-04 15:00:08 -0800

    react template, with babel and webpack integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/app.js",
+    devtool: 'source-map',
+    entry: './source/index.js',
     output: {
-        path: "./dist",
-        filename: "bundle.js"
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+      publicPath: '/assets/'
     },
+    module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query:
+        {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            },
-        }),
-        new HtmlWebpackPlugin({
-          title: 'xCoin',
-          filename: 'index.html'
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+        },
+        output: {
+          comments: false,
+        },
       })
     ]
 };
